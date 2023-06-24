c919b6c12d82a28fa8ce0208e5e85276e3ee46cf
Rina Inada <indrn@icloud.com>
2023-06-24 02:12:13 +0000

    Set ejs as view engine

--- express_server.js
+++ express_server.js
@@ -2,32 +2,35 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+//Set ejs as the view engine
+app.set("view engine", "ejs");
+
 const urlDatabase = {
- "b2xVn2": "http://www.lighthouselabs.ca",
- "9sm5xK": "http://www.google.ca", 
-}
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.ca",
+};
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`)
-})
+  console.log(`Example app listening on port ${PORT}!`);
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
 app.get("/hello", (rew, res) => {
-  res.send("<html><body>Hello <b>world</b></body></html>\n")
-})
+  res.send("<html><body>Hello <b>world</b></body></html>\n");
+});
 
 app.get("/set", (req, res) => {
- const a = 1;
- res.send(`a = ${a}`);
+  const a = 1;
+  res.send(`a = ${a}`);
 });
 
 app.get("/fetch", (req, res) => {
- res.send(`a = ${a}`);
+  res.send(`a = ${a}`);
 });
